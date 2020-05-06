a88825f77fa1435424fd409c0effb3df5ed15c9f
kmccall707 <kmccall@opentable.com>
2020-05-06 10:48:11 -0700

    fixing API Calls

--- manage-slack.py
+++ manage-slack.py
@@ -5,10 +5,12 @@ import requests
 import argparse
 import datetime
 from pprint import pprint
+from datetime import datetime
 import json
 import csv
 import os
 from slack import WebClient
+import slack
 
 
 def parse_args(args=None, parse=True):
@@ -17,13 +19,24 @@ def parse_args(args=None, parse=True):
                     help = 'print a list of user activity')
     parser.add_argument('-r', '--remove-users', action = 'store_true', 
                         help = 'remove all inactive users from all public channels')
-
+    parser.add_argument('-m', '--member-name', help = 'input member name')
     args = parser.parse_args() if parse else None
     return parser, args
 
+def get_channels(client):
+    convos = client.conversations_list(types="public_channel,private_channel")
+    channels = []
+    for convo in convos:
+	    for channel in convo["channels"]:
+		    channels.append({
+                channel['name']: channel['id']
+            })
+    return(channels)
 
-def get_users(users):
+def get_users(client):
     user_report = {}
+    response = client.users_list()
+    users = response["members"]
     for user in users:
         if user['is_bot']:
             continue
@@ -35,54 +48,71 @@ def get_users(users):
             }
     return(user_report)
 
-def users_list(users):
-    users_info = {}
+#find specific user
+def user(client, member_name):
+    user_dict = {}
+    response = client.users_list()
+    users = response["members"]
     for user in users:
         if user['is_bot']:
             continue
         if user['deleted']:
             continue
-        users_info[user['real_name']] = {
-            'id': user['id']
-        }
-    return(users_info)
-         
+        if user['real_name'] == member_name:
+            user_dict[user['real_name']] = {
+                'id': user['id']
+            }
+    return(user_dict)
 
-def get_messages(channels, slack_token):  
+
+def channel_history(client, channel_id):
+    messages = []
+    response = client.conversations_history(channel = channel_id)
+    convos = response["messages"]
+    return(convos)
+
+
+def get_messages(channels, client):  
     message_report = {}  
     message_list = []
-    channel_list = []
     for channel in channels:
-        messages = channel_history(slack_token, channel['id'])
-        if channel['name'] not in channel_list:
-            channel_list.append(channel['name'])
-        for message in messages:
-            if message['type'] == "message" and not "subtype" in message:
-                message_report ={
-                    'channel': channel['name'],
-                    'id': message['user'],
-                    'time': float(message['ts'])
-                }
-                message_list.append(message_report)
+        for k,v in channel.items():
+            messages = channel_history(client, v)
+            for message in messages:
+                if message['type'] == "message" and not "subtype" in message:
+                    message_report ={
+                        'channel': k,
+                        'id': message['user'],
+                        'time': float(message['ts'])
+                    }
+                    message_list.append(message_report)
     return(message_list)
 
+
+#check presence of specific user
 def message_to_member(message_list, user_dict):
-    mes_member_list={}
+    member_messages = []
+    latest_message={}
     for m in message_list:
         for k,v in m.items():
                 for k,v in user_dict.items():
                     if m['id'] == v['id']:
-                        mes_member_list[k] = m['time']
-    return(mes_member_list)
+                        latest_message = {
+                            'name': k,
+                            'channel': m['channel'],
+                            "time" : datetime.fromtimestamp(m['time'])
+                        }
+                        member_messages.append(latest_message)
+    return(member_messages)
 
-
+#check activity for all users
 def check_activity(message_list, user_report):
     for m  in message_list:
         for k,v in m.items():
             for k,v in user_report.items():
                 if m['id'] == v['id']:
                     if m['time'] > v['time']:
-                        v['time'] = m['time']  
+                        v['time'] = m['time']
                     else:
                         pass
     return(user_report)
@@ -108,52 +138,6 @@ def report_to_csv(user_report):
             for data in status:
                 writer.writerow(data)
 
-API_METHODS = {
-    "users": "users.list",
-    "conversations": "conversations.list"
-}
-
-def channel_history(token, channel_id):
-    url = 'https://slack.com/api/conversations.history/'
-    headers = {
-        'Content-type': 'application/json'
-    }
-    params = {
-            "token": token,
-            "channel": channel_id,
-            # "limit": 1,
-            "pretty": 1
-
-        }
-    r = requests.get(url, headers=headers, params = params)
-    data = r.json()
-    return(data['messages'])
-
-def slack_api(method,  token):
-    url = f'https://slack.com/api/{API_METHODS[method]}'
-    headers = {
-        'Content-type': 'application/json'
-    }
-    if method == 'conversations':
-        params = {
-            "token": token,
-            "types": ["public_channel, private_channel"],
-            # "limit": 20,
-            "pretty": 1
-
-        }
-        r = requests.get(url, headers=headers, params = params)
-        data = r.json()
-        return(data['channels'])
-    else:
-        params = {
-            "token": token,
-            # "limit": 1,
-            "pretty": 1
-        }
-        r = requests.get(url, headers=headers, params = params)
-        data = r.json()
-        return(data['members'])
 
 # Removal, -r
 # if args.remove_users:
@@ -167,30 +151,44 @@ def slack_api(method,  token):
 #                     pass
 
 def main():
+    pprint("Starting script....")
     _parser, args = parse_args()
-    # slack_token = "xoxp-1007626892624-1007668785253-1103928365040-aae685df974363008448e3fc1de3395e"
-    slack_token = 'xoxb-1007626892624-1120960962288-AfPLSK9j1PdPS4qmpMxpCuCs'
-    oauth_token = 'xoxp-1007626892624-1009836989174-1120937463584-251acb947538d5651f224d2dff8729dc'
+    oauth_token = 'xoxp-1007626892624-1009836989174-1123151586272-6edac0ac2a32689397b7976eb5d5e76e'
     
-    if not slack_token:
-        print('You need to set the slack_token variable, see the readme file')
+    if not oauth_token:
+        print('You need to set the oauth_token variable, see the readme file')
         exit()
     if not args.list_activity and not args.remove_users:
         parser.print_help()
         exit()
 
-    # Gather Data
-    users = slack_api('users', slack_token)
-    channels = slack_api('conversations', slack_token) 
+    client = slack.WebClient(token=oauth_token)
+   
+    if args.member_name: 
+        athlete = user(client, args.member_name)
+        pprint("Collecting data for athlete: ")
+        pprint(athlete)
+        channels = get_channels(client)
+        messages_list = get_messages(channels, oauth_token)
+        pprint("Here is a list of athlete's messages and timestamps: ")
+        last_message = message_to_member(messages_list, athlete)
+        pprint("The athlete's most recent message was: ")
+        pprint(last_message)
 
-    user_reports = get_users(users)
-    user_info = users_list(users)
-    messages_list = get_messages(channels, oauth_token)
-    # user_to_message = message_to_member(messages_list, user_info)
-    updated_users = check_activity(messages_list, user_reports)
-    
-    if args.list_activity:
-        report_to_csv(updated_users)
+    else:
+        pprint("Collecting users...")
+        user_reports = get_users(client)
+        pprint("Collecting channels...")
+        channels = get_channels(client)
+        pprint("Collecting messages..")
+        messages_list = get_messages(channels, client)
+        pprint("Checking member activity..")
+        updated_users = check_activity(messages_list, user_reports)
 
+        pprint("Writing to csv file...")
+        if args.list_activity:
+            report_to_csv(updated_users)
+
+    pprint("Done!")
 
 main()
